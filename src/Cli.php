506dbdb5ad01b18eae8a3d01eba37c9b93fc893e
Axel <axel19911@gmail.com>
2024-03-27 13:09:41 +0200

    step 4

--- src/Cli.php
+++ src/Cli.php
@@ -5,11 +5,9 @@ namespace Brain\Games\Cli;
 use function cli\line;
 use function cli\prompt;
 
-
 function greeting()
 {
     line('Welcome to the Brain Game!');
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
 }
-
