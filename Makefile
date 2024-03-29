9c7db15b52665019a43b1c820022c0d751ddc34d
Axel <axel19911@gmail.com>
2024-03-29 13:10:05 +0200

    add brain-prime files, update composer.json, makefile

--- Makefile
+++ Makefile
@@ -12,5 +12,7 @@ brain-gcd:
 	./bin/brain-gcd
 brain-progression:
 	./bin/brain-progression
+brain-prime:
+	./bin/brain-prime
 lint:
 	composer exec --verbose phpcs -- --standard=PSR12 src bin
---
+++ src/Games/Game-Prime.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Brain\Games\Games\Game\Prime;
+
+use Brain\Games\Cli;
+use Brain\Games\Engine;
+
+use function cli\line;
+use function cli\prompt;
+
+function startPrime()
+{
+    
+}
